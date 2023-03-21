04bb550c2de687b7b2c25e71a402282f0d2be478
Jeffer <ing.jeffer@hotmail.com>
2023-03-20 21:01:09 -0500

    fix

--- src/app/modules/gestor/constants/estados-convenio.ts
+++ src/app/modules/gestor/constants/estados-convenio.ts
@@ -11,25 +11,21 @@ export interface IEstadoConvenioSecretaria {
 }
 
 export enum EstadoConvenioDirectorRelex {
-    Firmado = "FIRMADO",
     Aprobado = "APROBADO_DIRECTOR_RELEX",
     Rechazado = "RECHAZADO_DIRECTOR_RELEX"
 }
 
 export interface IEstadoConvenioDirectorRelex {
-    Firmado: string;
     Aprobado: string;
     Rechazado: string;
 }
 
 export enum EstadoConvenioConsejoAca {
-    Firmado = "FIRMADO",
     Aprobado = "APROBADO_CONSEJO_ACADEMICO",
     Rechazado = "RECHAZADO_CONSEJO_ACADEMICO"
 }
 
 export interface IEstadoConvenioConsejoAca {
-    Firmado: string;
     Aprobado: string;
     Rechazado: string;
 }
